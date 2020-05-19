aa8c76ed56a80c5e7c0f34dc25151b89f98dfe95
ktsn <ktsn55@gmail.com>
2020-05-20 01:55:21 +0800

    re-split tsconfig

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testRegex: '/test/.+\\.spec\\.(js|ts)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'test/tsconfig.json',
+    },
+  },
 }
